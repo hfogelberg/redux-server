fbc86658a7070520dd5cb3232da5d0c888e6f369
Henrik Fogelberg <henrik.fogelberg@gmail.com>
2016-11-04 10:43:12 +0000

    Routing set up

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,14 @@
+import React, {PropTypes, Component} from 'react';
+import classnames from 'classnames';
+import './style.css';
+
+export default class NotFound extends Component {
+  render() {
+    const {className, ...props } = this.props;
+    return (
+      <div className={classnames('NotFound', className)} {...props}>
+        <h1>404 Not Found</h1>
+        </div>
+    );
+  }
+}
