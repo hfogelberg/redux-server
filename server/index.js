5676dfa0b9ee713253d17247ae435fbe1e9e9e94
Henrik Fogelberg <henrik.fogelberg@gmail.com>
2016-11-04 14:47:10 +0000

    Readme added

--- server/index.js
+++ server/index.js
@@ -21,8 +21,12 @@ require('./api')(apiRouter);
 app.use('/api', apiRouter);
 
 // Default route
-app.get('*', function(req, res) {
- res.sendFile(path.join(__dirname + '/build/index.html'));
+// Serve static assets
+app.use(express.static(path.resolve(__dirname, '..', 'build')));
+
+// Always return the main index.html, so react-router render the route in the client
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
 });
 
 app.listen(port, function(err){
